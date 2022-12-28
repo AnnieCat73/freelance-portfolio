284b23d89952f087c02a10fb8f9bbe7dd9f04209
Annie  Indreiten <devannie777@gmail.com>
2022-12-28 16:30:20 +0000

    Links added

--- script.js
+++ script.js
@@ -13,6 +13,37 @@ closeBtn.addEventListener("click", () => {
   navMenu.classList.toggle("navigation-open");
 });
 
+//Navigate to sections from the mobile menu/
+
+const aboutLink = document.querySelector("#js-about");
+const servicesLink = document.querySelector("#js-services");
+const workLink = document.querySelector("#js-work");
+const contactLink = document.querySelector("#js-contact");
+
+//Get to about section 
+aboutLink.addEventListener("click", () => {
+  navMenu.classList.remove("navigation-open");
+});
+
+//Get to services section
+servicesLink.addEventListener("click", () => {
+  navMenu.classList.remove("navigation-open");
+});
+
+//Get to work/portfolio section
+workLink.addEventListener("click", () => {
+  navMenu.classList.remove("navigation-open");
+});
+
+//Get to contact section
+contactLink.addEventListener("click", () => {
+  navMenu.classList.remove("navigation-open");
+});
+
+
+
+
+
 //Form validation in Contact Section
 const form = document.querySelector("form[name='contact-form']");
 const nameInput = document.querySelector("input[name='name']");
