fa69172773e579e21d8113731f3eb08f21081c0e
Annie  Indreiten <devannie777@gmail.com>
2022-12-29 16:10:46 +0000

    Fade in animation on each section when scrolling page

---
+++ app.js
@@ -0,0 +1,36 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    //console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+})
+
+
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+/* Animation for each section while scrolling down the page*/
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
+
--- script.js
+++ script.js
@@ -42,7 +42,25 @@ contactLink.addEventListener("click", () => {
 
 
 
+///Animation for each section while scrolling down the page
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
 
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
 
 //Form validation in Contact Section
 const form = document.querySelector("form[name='contact-form']");
